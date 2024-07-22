45f9c55ca04cf45897221f7c965be70035bb1ee0
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2024-07-22 20:29:00 +0530

    Add boilerplate
    Signed-off-by: Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Hostel in Aligarh",
+  description: "Cheapest online booking for hostel in Aligarh",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
