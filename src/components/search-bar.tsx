f594f08f1b4903170b649e14e605a4d8741e2845
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2024-07-24 22:49:06 +0530

    chore: fix search bar and prettier fix

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,14 +1,24 @@
 import AddGuest from "@/components/add-guest";
 import DatePickerWithRange from "@/components/ui/date-picker";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
-export default function SearchBar() {
+export default function SearchBar({
+  className,
+}: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <>
-      <div className="lg:w-1/2 bg-white rounded-[40px] h-20 flex p-2">
+      <div
+        className={cn(
+          className,
+          "lg:w-1/2 bg-white rounded-[40px] h-16 flex shadow-xl border-gray-100 border-[1px]",
+        )}
+      >
         <DatePickerWithRange className="h-full rounded-[40px] w-1/3" />
         <AddGuest className="w-1/3" />
-        <Button className="w-1/3 h-full rounded-[40px]">Search</Button>
+        <Button className="w-1/3 h-full bg-red-500 rounded-[40px]">
+          Search
+        </Button>
       </div>
     </>
   );
