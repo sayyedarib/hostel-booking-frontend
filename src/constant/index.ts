b7508a21dab4778a8ea49b368798be67463c60f1
unknown <sayyidalijufri@gmail.com>
2024-07-29 16:47:21 +0700

    change image file format to webp

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,49 +1,49 @@
 export const services = [
   {
     title: "Tiffin Service",
-    icon: "/img/restraunt.png",
+    icon: "/img/restraunt.webp",
     description:
       "We ensure the student get food at their desk irrespective of whether.",
   },
   {
     title: "Dedicated Workspace",
-    icon: "/img/workspace.png",
+    icon: "/img/workspace.webp",
     description:
       "We provide dedicated workspace to our students. We ensure the student get a place to work.",
   },
   {
     title: "Purified Water",
-    icon: "/img/water.png",
+    icon: "/img/water.webp",
     description:
       "We provide purified water to our students. We ensure the student get clean water.",
   },
   {
     title: "Laundry Service",
-    icon: "/img/laundry.png",
+    icon: "/img/laundry.webp",
     description:
       "We provide laundry service to our students. We ensure the student get clean clothes.",
   },
   {
     title: "Room Cleaning",
-    icon: "/img/cleaning.png",
+    icon: "/img/cleaning.webp",
     description:
       "We provide room cleaning service daily to our students. We ensure the student get clean room.",
   },
   {
     title: "Wi-Fi",
-    icon: "/img/wifi.png",
+    icon: "/img/wifi.webp",
     description:
       "We provide Wi-Fi service to our students. We ensure the student get high speed internet.",
   },
   {
     title: "24/7 Support",
-    icon: "/img/support.png",
+    icon: "/img/support.webp",
     description:
       "We provide 24/7 support service to our students. We ensure the student get support.",
   },
   {
     title: "Security",
-    icon: "/img/security.png",
+    icon: "/img/security.webp",
     description:
       "We provide security service to our students. We ensure the student get secure environment",
   },
@@ -56,10 +56,10 @@ export const services = [
 ];
 
 export const gallery = [
-  "/img/rooms/room0.jpg",
-  "/img/rooms/room1.jpeg",
-  "/img/rooms/room2.jpeg",
-  "/img/rooms/room3.jpeg",
-  "/img/rooms/room4.jpeg",
-  "/img/rooms/room5.jpeg",
+  "/img/rooms/room0.webp",
+  "/img/rooms/room2.webp",
+  "/img/rooms/room1.webp",
+  "/img/rooms/room3.webp",
+  "/img/rooms/room4.webp",
+  "/img/rooms/room5.webp",
 ];
