59e2dc4ffab279133a602de452e39019bf2bd16a
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2024-08-01 22:24:29 +0530

    chore: form ui fix

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -2,7 +2,7 @@ import PayingGuestAgreement from "@/components/form";
 
 export default function Form() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="max-w-screen min-h-screen flex items-center justify-center mt-10">
       {" "}
       <PayingGuestAgreement />
     </div>
