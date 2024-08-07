929b26e0648a297a484d12baf106bd3a1d073ca4
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2024-08-07 11:02:28 +0530

    add dropdown component and thanks page

--- src/app/thanks/page.tsx
+++ src/app/thanks/page.tsx
@@ -1,7 +1,9 @@
 export default function Thanks() {
-    return (
-        <div className="flex flex-col gap4 min-w-screen min-h-screen items-center justify-center">
-        <h1 className="text-3xl md:text-6xl text-red-500 font-bold">Thank you!</h1>
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap4 min-w-screen min-h-screen items-center justify-center">
+      <h1 className="text-3xl md:text-6xl text-red-500 font-bold">
+        Thank you!
+      </h1>
+    </div>
+  );
 }
