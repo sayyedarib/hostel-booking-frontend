876b12a9d1347cdfbd3144135f797d3bc643d8f8
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2024-08-25 22:57:13 +0530

    remove bg from facilities card
    Signed-off-by: Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>

--- src/components/landing-page/facilities.tsx
+++ src/components/landing-page/facilities.tsx
@@ -4,10 +4,10 @@ const facilities = [
     title: "24/7 Reception",
     icon: "/icons/24_7.png",
   },
-  {
-    title: "Air Cooler",
-    icon: "/icons/air.png",
-  },
+  // {
+  //   title: "Air Cooler",
+  //   icon: "/icons/air.png",
+  // },
   {
     title: "CCTV Surveillance",
     icon: "/icons/cc.png",
@@ -16,10 +16,10 @@ const facilities = [
     title: "Purified Water",
     icon: "/icons/ro.png",
   },
-  {
-    title: "Food",
-    icon: "/icons/food.png",
-  },
+  // {
+  //   title: "Food",
+  //   icon: "/icons/food.png",
+  // },
   {
     title: "Gyeser",
     icon: "/icons/gyeser1.png",
@@ -40,7 +40,7 @@ export default function Facilities() {
       {facilities.map((facility, index) => {
         return (
           <div key={index} className="flex flex-col items-center mt-10 gap-3">
-            <div className="w-40 h-40 mx-auto flex md:w-60 md:h-60 items-center justify-center gap-2 rounded-full p-1 bg-[#7091E6]">
+            <div className="w-40 h-40 mx-auto flex md:w-60 md:h-60 items-center justify-center gap-2 rounded-full p-1">
               <Image src={facility.icon} width={150} height={150} alt="" />
             </div>
             <span className="font-extrabold"> {facility.title}</span>
