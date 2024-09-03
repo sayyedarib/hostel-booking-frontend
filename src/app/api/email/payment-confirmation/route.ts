7a1504cc7a91067d7409a5d0e2ef496ae253321b
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2024-09-03 07:42:53 +0530

    fix redirect in payment confirmation
    Signed-off-by: Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>

--- src/app/api/email/payment-confirmation/route.ts
+++ src/app/api/email/payment-confirmation/route.ts
@@ -27,7 +27,7 @@ export async function GET(request: NextRequest) {
       .execute();
 
     logger("info", "Payment confirmed");
-    return NextResponse.redirect("/payment-confirmed");
+    return NextResponse.redirect(new URL('/payment-confirmed', request.url))
   } catch (error) {
     logger("error", "Failed to confirm payment", error as Error);
     return NextResponse.json(
