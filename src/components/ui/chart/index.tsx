c567e5891b5c62e3c6c565b9612c2dbc8d21a095
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2024-09-12 15:15:51 +0530

    add shadcn charts

---
+++ src/components/ui/chart/index.tsx
@@ -0,0 +1,5 @@
+import { BarChartComponent } from "./bar-chart";
+import { PieChartComponent } from "./pie-chart";
+import { LineChartComponent } from "./line-chart";
+
+export { BarChartComponent, PieChartComponent, LineChartComponent };
