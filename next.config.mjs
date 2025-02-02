2695b638b9c3690d287ceac242e4c555dd6cea4d
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2025-02-02 15:27:51 +0530

    update supbase public url
    Signed-off-by: Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "ilfpkquwhyjixgcvupye.supabase.co",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ihrzclenndzybhyrrtko.supabase.co",
+        hostname: "jiikjsxvdowyylhgckjy.supabase.co",
         port: "",
         pathname: "/**",
       },
